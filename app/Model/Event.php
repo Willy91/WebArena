256392d736cdd07dbd43e8dfc256f80768918a3e
MatthieuBlais <blais@ece.fr>
2014-11-24 00:25:10 +0100

    Add of the Event model

---
+++ app/Model/Event.php
@@ -0,0 +1,135 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+class Event extends AppModel {
+    
+    
+    
+    function doAttackEvent($idFighter, $idDefender){
+        $data = $this->Fighter->findById($idFighter);
+        $data2 = $this->Fighter->findById($idDefender);
+        
+        $name = $data['Fighter']['name'] . " attaque " . $data2['Fighter']['name'] . " et le touche";
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function failAttackEvent($idFighter, $idDefender){
+        $data = $this->Fighter->findById($idFighter);
+        $data2 = $this->Fighter->findById($idDefender);
+        
+        $name = $data['Fighter']['name'] . " attaque " . $data2['Fighter']['name'] . " mais le rate";
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function newFighterEvent($idFighter){
+        $data = $this->Fighter->findById($idFighter);
+        
+        $name = "Entrée de " . $data['Fighter']['name'];
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function getToolEvent($idFighter, $idTool){
+        $data = $this->Fighter->findById($idFighter);
+        $data2 = $this->Tool->findById($idTool);
+        
+        $name = $data['Fighter']['name'] . " ramasse un nouvel objet : " . $data2['Tool']['type'];
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function joinGuildEvent($idFighter, $idGuid){
+        $data = $this->Fighter->findById($idFighter);
+        $data2 = $this->Guild->findById($idGuid);
+        
+        $name = $data['Fighter']['name'] . " rejoint " . $data2['Guild']['name'];
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    
+    function newGuildEvent($idFighter, $nameGuild){
+        $data = $this->Fighter->findById($idFighter);
+        
+        $name = $data['Fighter']['name'] . " crée une nouvelle guilde sous le nom de " . $nameGuild;
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function newDeathEvent($idFighter){
+        $data = $this->Fighter->findById($idFighter);
+        
+        $name = $data['Fighter']['name'] . " est mort ";
+        
+        $new = $this->create();
+        $new['Event']['name'] = $name;
+        $new['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $new['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $new['Event']['date'] = date("d-m-Y H-i-s");
+        $this->save($new);
+        
+    }
+    
+    function Crier($idFighter, $Name){
+        
+        $data = $this->Fighter->findById($idFighter);
+        
+        $data2 = $this->create();
+        $data2['Event']['name'] = $Name;
+        $data2['Event']['coordinate_x']=$data['Fighter']['coordinate_x'];
+        $data2['Event']['coordinate_y']=$data['Fighter']['coordinate_y'];
+        $data2['Event']['date'] = date("d-m-Y H:i:s");
+        $this->save($data2);
+    }
+    
+    
+    //In order to get the events in last 24 hours.
+    function getEvent(){
+        
+        $date_lim = date ("Y-m-d H:i:s", mktime(date("H"),date("i"),date("s"),date("m"),date("d")-1,date("Y")));
+        
+        $tab = $this->find('all', array('conditions' => array ("date >" => $date_lim)));        
+        
+        return $tab;
+    }
+    
+}
